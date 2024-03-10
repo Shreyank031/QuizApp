3b139bedac16efe7be888787d975d1fbe3afdf05
Shreyank031 <shreyankchandu@gmail.com>
2024-03-10 11:34:04 +0530

    Add Firestore setup to MainActivity
    - Set up Firebase Firestore instance in MainActivity
    - Added functionality to retrieve data from Firestore collection user31
    - Implemented logging of retrieved Firestore data for debugging purposes
    
    This commit adds the necessary code to configure Firebase Firestore in the MainActivity class. It includes setting up the Firestore instance, accessing a specific Firestore collection, and logging retrieved data for debugging purposes.

--- quizzy/app/src/main/java/com/shrey/quizzy/activity/MainActivity.kt
+++ quizzy/app/src/main/java/com/shrey/quizzy/activity/MainActivity.kt
@@ -2,12 +2,15 @@ package com.shrey.quizzy.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.firestore.FirebaseFirestore
 import com.shrey.quizzy.R
 import com.shrey.quizzy.adapters.QuizAdapter
 import com.shrey.quizzy.models.Quiz
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     // Late-initialized properties
     lateinit var drawerToggle: ActionBarDrawerToggle
     lateinit var adapter: QuizAdapter
+    lateinit var fireStore: FirebaseFirestore
 
     var quizList = mutableListOf<Quiz>()
 
@@ -27,9 +31,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setView() {
+        setUpFireStore()
         setDrawerToggle()
         populateData()
         setRecyclerView()
+
+    }
+    // Method to set up Firebase Firestore
+    private fun setUpFireStore() {
+        fireStore = FirebaseFirestore.getInstance() // Get Firestore instance
+        val collections = fireStore.collection("user31") // Access "user31" collection
+        collections.addSnapshotListener { value, error ->
+            if (value == null || error != null) {
+                Toast.makeText(this, "Error while retrieving data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            //'value' is the QuerySnapshot object obtained from the Firestore database.
+            //convert firestore documents into Quize objects
+            Log.d("DATAA", value.toObjects(Quiz::class.java).toString()) // Log retrieved data
+        }
+
     }
 
     // Method to populate data into the quiz list
--- quizzy/app/src/main/java/com/shrey/quizzy/models/Quiz.kt
+++ quizzy/app/src/main/java/com/shrey/quizzy/models/Quiz.kt
@@ -3,6 +3,6 @@ package com.shrey.quizzy.models
 data class Quiz(
     val id: String = "",
     val title: String = "",
-    val question: MutableMap<String, Question> = mutableMapOf()
+    val questions: MutableMap<String, Question> = mutableMapOf()
 
 )
